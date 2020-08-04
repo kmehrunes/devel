8f3a073f1ed7acd911ef27b61f34cc868caf97c3
Khaled Y.M <kld_at7@hotmail.com>
2020-08-04 20:09:47 +0100

    Jackson: get mapper for a file

--- jackson/src/main/java/io/devel/jackson/ObjectMappers.java
+++ jackson/src/main/java/io/devel/jackson/ObjectMappers.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.util.Optional;
+
 public class ObjectMappers {
     public static ObjectMapper yamlMapper() {
         return configureMapper(new ObjectMapper(new YAMLFactory()));
@@ -16,5 +18,15 @@ public class ObjectMappers {
     public static ObjectMapper configureMapper(final ObjectMapper objectMapper) {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
+    }
+
+    public static Optional<ObjectMapper> getForFile(final String fileName) {
+        if (fileName.endsWith(".yml") || fileName.endsWith(".yaml")) {
+            return Optional.of(yamlMapper());
+        } else if (fileName.endsWith(".json")) {
+            return Optional.of(jsonMapper());
+        }
+
+        return Optional.empty();
     }
 }
